4470555ceea15f0ccf0e4375aaba26339541a8cb
Andrés Rosales <Andrescod10@gmail.com>
2024-02-27 02:42:23 -0400

    Finalizando header nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,12 +7,18 @@ module.exports = {
 	theme: {
 		extend: {
 			colors:{
-				accent:{
+				accent: {
+					1: "hsl(288 95.8% 90.6%)",
+					2: "hsl(168 83.8% 78.2%)",
+				},
+				bkg: "hsl(210 40% 98%)",
+				content: "hsl(217 32.6% 17.5%)",
+				/* accent:{
 					1: "hsl(var(--color-accent1) / <alpha-value>)",
 					2: "hsl(var(--color-accent2) / <alpha-value>)"
 				},
 				bkg: "hsl(var(--color-bkg) / <alpha-value>)",
-				content: "hsl(var(--color-content) / <alpha-value>)"
+				content: "hsl(var(--color-content) / <alpha-value>)" */
 			},
 			animation: {
 				"spin-lower": "spin 35s ease infinity",
