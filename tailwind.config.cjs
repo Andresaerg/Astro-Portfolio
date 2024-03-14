9ca956309d49030aa663e555996fe045b10a79a8
Andrés Rosales <Andrescod10@gmail.com>
2024-03-13 23:26:09 -0400

    Updating techs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,26 +8,7 @@ module.exports = {
 		fontFamily: {
 			'anta' : ["Anta", "sans-serif"]
 		},
-		extend: {
-			colors:{
-				accent: {
-					1: "hsl(288 95.8% 90.6%)",
-					2: "hsl(168 83.8% 78.2%)",
-				},
-				bkg: "hsl(210 40% 98%)",
-				content: "hsl(217 32.6% 17.5%)",
-				/* accent:{
-					1: "hsl(var(--color-accent1) / <alpha-value>)",
-					2: "hsl(var(--color-accent2) / <alpha-value>)"
-				},
-				bkg: "hsl(var(--color-bkg) / <alpha-value>)",
-				content: "hsl(var(--color-content) / <alpha-value>)" */
-			},
-			animation: {
-				"spin-lower": "spin 35s ease infinity",
-				"spin-slow": "spn 25s ease-in-out infinite reverse",
-			}
-		},
+		extend: {},
 	},
 	plugins: [
 		require('flowbite/plugin'),
