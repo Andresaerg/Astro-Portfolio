ee609df832ca71f462a9e49095698dccfc9e2e8a
Andrés Rosales <Andrescod10@gmail.com>
2024-06-21 05:14:07 -0400

    Finished and deployed in surge

---
+++ src/js/downloadFile.js
@@ -0,0 +1,34 @@
+function DownloadFile(fileName) {
+    //Set the File URL.
+    var url = "https://andresaerg.github.io/Astro-Portfolio/assets/" + fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            let link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
+
+document.getElementById('cv-link').addEventListener('click', function(event){
+    event.preventDefault();
+    DownloadFile(this.getAttribute('data-download'))
+})
